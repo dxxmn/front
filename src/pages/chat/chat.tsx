adc2ef78fe9301c414db0555f405e931fc5eeb02
dxmn <artem_mochalov_2006@mail.ru>
2024-11-28 09:14:54 +0300

    f

--- src/pages/chat/chat.tsx
+++ src/pages/chat/chat.tsx
@@ -1,11 +1,95 @@
-import React from 'react';
+import { Button, Card, CardBody, Navbar, NavbarContent, NavbarItem, Textarea, User } from '@nextui-org/react';
+import React, { useContext } from 'react';
+import { FaRegMoon, FaRegUserCircle, FaRobot } from 'react-icons/fa';
+import { LuSunMedium } from 'react-icons/lu';
+import { ThemeContext } from '../../components/theme-provider';
 
-export const Chat = () => {
+export const Chat = (color:string) => {
+    const { theme, toggleTheme } = useContext(ThemeContext)
+    const messages = [{ ai: "asdasdsd", user: "asddfgdh" },
+    { ai: "hryjhfjg", user: "qrewter" },
+    { ai: "fasadaa", user: "tryetur" },
+    { ai: "fasadaa", user: "tryetur" },
+    { ai: "fasadaa", user: "tryetur" },
+    { ai: "fasadaa", user: "tryetur" },
+    { ai: "fasadaa", user: "tryetur" },
+    { ai: "fasadaa", user: "tryetur" },
+    { ai: "fasadaa", user: "tryetur" },
+    { ai: "fasadaa", user: "tryetur" },
+    { ai: "fasadaa", user: "tryetur" },
+    { ai: "fasadaa", user: "tryetur" },
+    { ai: "fasadaa", user: "tryetur" },
+    { ai: "fasadaa", user: "tryetur" },
+    { ai: "fasadaa", user: "tryetur" },
+    { ai: "fasadaa", user: "tryetur" },
+    { ai: "fasadaa", user: "tryetur" },
+    ]
 
 
     return (
-        <div className='flex flex-row max-w-full mx-5 mt-5 min-h-[87vh]'>
-            chat
+        <div className="overflow-x-hidden"  >
+            <Navbar maxWidth="full" isBordered className='fixed top-0 left-0 '>
+                <p className="font-bold text-inherit">C-SHARK</p>
+                <NavbarContent justify="end">
+                    <NavbarItem
+                        className="lg:flex text-3xl cursor-pointer flex justify-end"
+                        onClick={toggleTheme}
+                    >
+                        {theme === 'light' ? <FaRegMoon /> : <LuSunMedium />}
+                    </NavbarItem>
+                </NavbarContent>
+            </Navbar>
+
+            <div className="flex flex-col max-w-full mx-5 mt-5 min-h-screen relative">
+                <div className="flex max-w-full mt-5">
+                    <div className="w-full">
+                        {messages.map((m, index) => (
+                            <div key={index} className="flex flex-col w-full mb-4">
+                                {/* Сообщение от Assistant */}
+                                <div className="flex justify-start w-full">
+                                    <Card className=" flex w-full max-w-2xl h-32">
+                                        <div className="flex items-center gap-2">
+                                            <FaRobot className="size-14 p-2" />
+                                            <p>Assistant</p>
+                                        </div>
+                                        <CardBody className="flex-1 text-xl">
+                                            {m.ai}
+                                        </CardBody>
+                                    </Card>
+                                </div>
+                                {/* Сообщение от You */}
+                                <div className="flex justify-end w-full mt-2">
+                                    <Card className=" flex w-full max-w-2xl h-32">
+                                        <div className="flex items-center gap-2">
+                                            <FaRegUserCircle className="size-14 p-2" />
+                                            <p>You</p>
+                                        </div>
+                                        <CardBody className="flex-1 text-xl">
+                                            {m.user}
+                                        </CardBody>
+                                    </Card>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                
+                {/* Фиксированная нижняя панель ввода */}
+                <div className="fixed bottom-0 left-0 w-full z-10 ">
+                    <div className="flex justify-center px-5 py-4">
+                        <div className="w-full max-w-4xl">
+                            <Card>
+                                <CardBody className=" flex flex-row items-center">
+                                    <Textarea size="lg" minRows={3}  className="flex-1 text-lg" />
+                                    <Button size="lg" className="ml-4 text-lg ">
+                                        Задать вопрос
+                                    </Button>
+                                </CardBody>
+                            </Card>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 };
--- src/pages/export/index.tsx
+++ src/pages/export/index.tsx
@@ -1,9 +1,9 @@
-import { Card, CardBody, Select, SelectItem } from '@nextui-org/react';
+import { Card, CardBody, Select, SelectItem, Textarea } from '@nextui-org/react';
 import React, { useState } from 'react';
 
 export const Export = () => {
     const colors = ['Blue', 'Purple']
-    const [color,setColor] = useState('')
+    const [color, setColor] = useState('')
 
 
     return (
@@ -28,44 +28,23 @@ export const Export = () => {
                                     </Select>
                                 </CardBody>
                             </Card>
-                            <Card>
-                                <CardBody>
-                                <Select
-                                        defaultSelectedKeys={[color]}
-                                        label="Select an color"
-                                        className="max-w-xs"
-                                        onChange={(e) => setColor(e.target.value)}
-                                    >
-                                        {colors.map((color) => (
-                                            <SelectItem key={color}>
-                                                {color}
-                                            </SelectItem>
-                                        ))}
-                                    </Select>
-                                </CardBody>
-                            </Card>
-                            <Card>
-                                <CardBody>
-                                    asd
-
-                                </CardBody>
-                            </Card>
-                            <Card>
-                                <CardBody>
-                                    asd
-
-                                </CardBody>
-                            </Card>
+                            
                         </div>
                     </CardBody>
                 </Card>
 
             </div>
-            <div className='flex basis-3/4'>
+            <div className='flex flex-col basis-3/4'>
+                <Card className='flex w-full mb-3'>
+                    <CardBody>
+                        <Textarea value={'http://localhost:5173/chat'} isReadOnly minRows={1} maxRows={1}/>
+                    </CardBody>
+                </Card>
                 <Card className='flex w-full'>
                     <CardBody>
+
                         <div >
-                            <iframe src='http://localhost:5173/chat' width={1000} height={650}></iframe>
+                            <iframe src='http://localhost:5173/chat' className='overflow-x-clip' width={1000} height={580}></iframe>
                         </div>
                     </CardBody>
                 </Card>
